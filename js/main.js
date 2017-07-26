4655f6d3ac191e0d2d8345283b39ad1c41382ab6
Alina Palomaria <alinaguzman.yearup@gmail.com>
2017-07-26 05:06:36 -0700

    second

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 //upon click of the button the user name and comments are stored here in console
 
 function getInfo() {
-  var username = document.getElementById("username");
-  var comments=document.getElementById("area");
-  console.log("username: " + username.value + " area: " + area.value);
+  var username = document.getElementById("username").value;
+  var comments=document.getElementById("area").value;
+
+  var userNameText= document.createTextNode("username: " + username)
+  var commentText=document.createTextNode("Comment: " + comments)
 
   var newH1=document.createElement("H1");
   // var text=document.createTextNode(username.value);
+  newH1.appendChild(userNameText)
   var right =document.getElementById("right");
-  var newArea=document.createElement("H2");
+  var newArea=document.createElement("p");
+  newArea.appendChild(commentText)
 //  newArea=document.getElementById("area")
   var newContatiner=document.createElement("div");
   newContatiner.id=("addOn")
